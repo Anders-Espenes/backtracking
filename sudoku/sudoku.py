54dc7b118a9e6f8cb1d9c6872b8eb06d53769ee5
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2021-08-31 17:46:57 +0200

    Added counters

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,42 +1,11 @@
 # Problem 1 Sudoku Backtracking solver
-'''
 
-A  Sudoku  puzzle  consists  of  9  3x3  squares  arranged  as  shown  on  the  next  page.    
-Some  squares  are  initialized with numbers from 1 to 9 while others are left blank. 
-The objective is to fill all squares with numbers from 1 to 9 such that each number occurs exactly once in each row, each column and each 3x3 square.
-1.    Implement a backtracking solution.
-2.    Count the number of visited and promising nodes in the search tree.
-3.    Implement  some  deductive  strategies  (e.g.  x-wing,  swordfish,  forcing  chain,  etc)  to  reduce  the  search space. 
-        How much do they contribute towards making the search for a solution faster?
-'''
-
-''' 
-Backtracking methodology 
-Backtracking is a modified depth-first search of a rooted tree
-Root node is visited first, a visit to a node is followed immediately by visits to ALL child nodes
-Solves problems a sequence of objects is chosen from a specified set so that the sequence satisfies some criterion.
-Identify what is the sequence
-Identify what is the set
-Identify what is the criterion
-    Identify promising nodes
-    Identify non promising nodes
-    Prune the tree, if a node is nonpromising backtrack as the children are not promising
-    
-
-
-Queens problem
-Sequence is the number of n positions in which the queen is placed
-The set is n^2 possible positions on the chessboard
-The criterion is that no two queens can threaten each other
-
-Sudoku:
-    Sequence is the number of positions in which 
-'''
-# IMPORTS
-from time import perf_counter_ns
 # GLOBALS
 SIZE = 9
 EMPTY = 0
+
+visited = 0
+promisingCounter = 0
 
 # Check axis for same value
 def check_axis(board, row, column, value):
@@ -65,9 +34,13 @@ def promising(board, row, column, value):
     return False # No input is valid
 
 def solve_sudoku(board):
+    global visited
+    visited += 1
     cell = find_zero(board)
     if(cell != False):
         for i in range (1, SIZE + 1):
+            global promisingCounter
+            promisingCounter += 1
             if(promising(board, cell[0], cell[1], i)):
                 board[cell[0]][cell[1]] = i # Update board
                 if(solve_sudoku(board)):
@@ -104,8 +77,6 @@ if __name__ == "__main__":
                 [1, 3, 8, 9, 4, 7, 2, 5, 6],
                 [6, 9, 2, 3, 5, 1, 8, 7, 4],
                 [7, 4, 5, 2, 8, 6, 3, 1, 9]]
-    # Start a timer
-    start_time = perf_counter_ns()
 
     # Solve the sudoku
     board = solve_sudoku(board)
@@ -114,5 +85,6 @@ if __name__ == "__main__":
     else:
         print_board(board)
         print("No Solution exists")
-    time = perf_counter_ns() - start_time
-    print("Time: ", time, "nano second")
+
+    print("Visited: ", visited)
+    print("Promising: ", promisingCounter)
