f0eb704a5ac3bc96e5ba64fa14c375bdaaaae588
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2021-09-11 19:07:09 +0200

    Generates gray codes with backtracking

--- greyCodes/greycodes.py
+++ greyCodes/greycodes.py
@@ -1,52 +1,7 @@
 # Problem 4 Greynodes something something
-'''
-Grey Nodes:
-    is an ordering of the binary numeral system such that:
-        Two successive values DIFFER in only one bit
-
-Problem 4
-Consider the problem of generating N-bit Grey codes: 
-    Given a number N, generate bit patterns from 0 to 2N-1such that successive patterns differ by one bit:  
-    ̈Input: N = 2 Output: 00 01 11 10
-    Input: N = 3 Output: 000 001 011 010 110 111 101 100
-    1.    Implement a backtracking solution.
-    2.    Count the number of visited and promising nodes in the search tree.
-    3.    Not every problem that looks like a constraint satisfaction problem is one that may need backtracking: 
-          n-bit  Gray  Codes  can  be  generated  from  list  of  (N-1)-bit  
-          Gray  codes  using  the  following  greedy algorithm  
-          (we  need  to  show  this  algorithm  generates  all  Gray  codes  and  nothing  else!). 
-    Implement the following algorithm:
-        a)  Let the list of (N-1)-bit Gray codes be L1. Create another list L2 which is reverse of L1.
-        b)  Modify the list L1 by prefixing a ‘0’ in all codes of L1.
-        c)  Modify the list L2 by prefixing a ‘1’ in all codes of L2.
-        d)  Concatenate L1 and L2. The concatenated list is required list of n-bit Gray 
-            codesHow much faster does the greedy algorithm find a solution? 
-            Can you give an (inductive) proof of the correctness of this algorithms?
-
-'''
-
-
-'''
-Problem Notes:
-
-Given a number N, generate bit patterns from 0 to 2^(N-1) such that successive patterns differ by one bit
-
-Input N = 2                 Input: N = 3
-Output: 00 01 11 10         Output: 000 001 011 010 110 111 101 100
-
-'''
-
-''' 
-Iterative method
-Each bit is inverted if the next higher bit of the input value is set to one
-'''
-
-
 from typing import Counter
+from copy import deepcopy
 
-
-def makeroot(bits):
-    return [tuple([0 for i in range(bits)])]
 
 # Performs an XOR operaton on two lists, lists must be same length
 # * does not append the 0 to the returned list, as we only want the 1s from the XOR operation
@@ -66,6 +21,7 @@ def distance(code):
     return True
 
 # Check if every pattern is unique
+# TODO: Change it to only check if one pattern is unique not the whole thing
 def unique(code):
   temp = Counter(map(tuple, code)) # Counts the occurence of item in list of lists, creates a tuple, item is a pattern, value is nr of occurences
   return next((False for v in temp.values() if v != 1), True) # If any item have more than 1 occurence return True, else return false
@@ -74,36 +30,51 @@ def unique(code):
 # Reject If: somthing is not uniqe or distance between adjacent patterns is greater than 1
 def reject(code):
     if(unique(code) and  # True if no pattern have occurence greater than 1
-    distance(code)): # Check if distance between patterns == 1
+    distance(code)): # True if distance between adjacent patterns == 1
         return True
     return False
 
-# TODO:
-# Check if all patterns have been found
+
+# Takes in copy of pattern list, checks if value is promising
+def promising(temp, pattern, bit, i):
+    if (len):
+        pattern.append(i)
+    else:
+        pattern.insert(bit, i)
+    temp.append(pattern)
+    if(unique(temp)): # Test value
+        return True # Value is promising
+    return False # Value is not promising
+
+
+def extensions(code):
+    for i in range(len(code[-1]) - 1, -1, -1): # Start backwards
+        nextcodepoint = list(code[-1])  # Create copy
+        nextcodepoint[i] = nextcodepoint[i] + 1 - nextcodepoint[i] * 2
+        yield code + [nextcodepoint]
+
 def accept(codelen, code):
- if (len(code[-1]) < codelen):
-     pass
+    if len(code) != codelen: # Check if the number of patterns is correct
+        return False
+    return (len(xor(code[0], code[-1])) == 1) # Check if distance between first and last pattern is 1
 
-# TODO:
-def output(code):
-    print("code: ")
-    for codepoint in code:
-        for codeelement in codepoint:
-            codechar = "1" if codeelement else "0"
-            print(codechar),
-        print
+def promising(pattern):
+    return reject(pattern)
 
+# Finishes when all patterns are unique and one bit shift difference with adjacent patterns
 def backtrack(codelen, code):
-    if not reject(code): # If code is not
-       return
-    if accept(codelen, code):
-   #     output(code)
-        return code
-   # for extension in code:
-   #     backtrack(codelen, extension)
-    return "No solution found"
+    if not reject(code): # Check if pattern fullfilles the requirements for grey code
+        return
+    if accept(codelen, code): # Check if finished
+        print_list(code)
+        exit() # TODO: Find a better solution to this, is not good coding
+    for extension in extensions(code):
+        if backtrack(codelen, extension):
+            return extension
 
-#TODO: BUG: With N=4 generates [0,1,0,0] then [1,1,0,1] should be [1,1,0,0]
+
+    
+
 # Uses a reflect and prefix method to generate grey code
 def reflective_greycode_generation(n, L1=[[]]):
     if(len(L1[-1]) < n):  # Stop when we have reached the desired number of bits
@@ -123,11 +94,13 @@ def chunk_list(list, n):
     return([list[i:i + n] for i in range(0, len(list), n)])
 
 
-if __name__ == '__main__':
-    n = 5
+def makeroot(bits):
+    return [[0 for _ in range(bits)]]
 
-    test = chunk_list([0,0,0,0,0,1,0,1,1,0,1,0,1,1,0,1,1,1,1,0,1,1,0,0], 3)
-    test2 = chunk_list([0,0,0,0,0,0,0,1,0,0,1,1,0,0,1,0,0,1,1,0,0,1,1,1,0,1,0,1,0,1,0,0,1,1,0,0,1,1,0,1,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1,0,0,1,1,0,0,0], 4)
+
+if __name__ == '__main__':
+    n = 2
+
     temp = reflective_greycode_generation(n)
     print_list(temp)
     if(distance(temp) and unique(temp)):
@@ -135,11 +108,4 @@ if __name__ == '__main__':
     else: 
      print("Reflective Failure")
 
-   #temp = backtrack(n,[[0],[1]])
-   #print_list(temp)
-   #if(temp == test):
-   #    print("Backtracking Success")
-   #else:
-   #    print("Backtracking Failure")
-   # code = [[0, 1, 0, 0], [1, 1, 0, 0]]
-    #print(backtrack(2, code))
+    backtrack(2**n, makeroot(n))
